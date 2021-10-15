b3b5f26658b8be4bf978d15e3a61e720254b720a
student-09-525 <student-09-525@ncfu.net>
2021-10-15 15:54:59 +0300

    commit 1

---
+++ Algorythm_1/Algorythm_1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+struct tv
+{
+	char[10] made;
+	char[10] model;
+	short int diag;
+	enum class res :char { HD, FullHD, UltraHD };
+}
+int Main()
+{
+	cout << sizeof(tv) << " byte(s)";
+	return 0;
+}
